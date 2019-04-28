95ee49aa4401c4f79d4572362f4c9c84a5225051
Jacob Ingalls <jacobingalls@yahoo.com>
2019-04-28 11:00:42 -0700

    COINTS!!!

---
+++ ProjectAlmond/Assets/Scripts/CoinDropper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinDropper : MonoBehaviour
+{
+
+    public GameObject coinPrefab;
+
+    List<GameObject> coins = new List<GameObject>();
+
+    public bool isDispensing;
+    
+    [Range(0.001f, 1.0f)]
+    public float iterval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float time = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDispensing) {
+            time += Time.fixedDeltaTime;
+            if (time > iterval) {
+                time -= iterval;
+
+                var coin = Instantiate(coinPrefab);
+                coin.transform.position = this.transform.position;
+                coins.Add(coin);
+            }
+        }
+    }
+}
