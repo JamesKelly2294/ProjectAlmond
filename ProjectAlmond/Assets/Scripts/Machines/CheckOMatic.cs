d6fbca92156dcf8828468e770b728f81a35b53d3
Jacob Ingalls <jacobingalls@yahoo.com>
2019-04-27 23:24:39 -0700

    Add the Check-o-matic

---
+++ ProjectAlmond/Assets/Scripts/Machines/CheckOMatic.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckOMatic : MonoBehaviour
+{
+
+    public NixyTube[] tubes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tubes[0].setText("H");
+        tubes[1].setText("E");
+        tubes[2].setText("L");
+        tubes[3].setText("L");
+        tubes[4].setText("O");
+        tubes[5].setText("");
+        tubes[6].setText("WOR");
+        tubes[7].setText("LD!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ejectButtonWasPressed() {
+        StartCoroutine("showEjectMessage");
+    }
+
+    public IEnumerator showEjectMessage()
+    {
+
+        string str = "        EJECTED!        ";
+
+        for (var i = 0; i < 16; i ++) {
+            for (var j = 0; j < 8; j++) {
+                tubes[j].setText(""+str[i + j]);
+            }
+
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        tubes[0].setText("");
+        tubes[1].setText("");
+        tubes[2].setText("");
+        tubes[3].setText("");
+        tubes[4].setText("");
+        tubes[5].setText("");
+        tubes[6].setText("");
+        tubes[7].setText("");
+    }
+}
--- ProjectAlmond/Assets/Scripts/NixyTube.cs
+++ ProjectAlmond/Assets/Scripts/NixyTube.cs
@@ -8,28 +8,17 @@ public class NixyTube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        setText("ok");
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
-        System.Random rand = new System.Random();
-        int i = rand.Next(0, 100);
-        if ( i == 0) {
-            setText("LOW");
-        } else if (i == 1) {
-            setText("HIGH");
-        } else if (i % 27 == 1) {
-            setText("GOOD");
-        } else if (i % 17 == 0) {
-            setText("");
-        }
     }
 
-    void setText(string newValue) {
+    public void setText(string newValue) {
         var light = this.GetComponentInChildren<Light>();
-        if (newValue == "") {
+        if (newValue.Trim() == "") {
             light.enabled = false;
         } else if(!light.enabled) {
             light.enabled = true;
