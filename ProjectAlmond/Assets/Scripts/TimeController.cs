0355d9d5804ad8c6c9962ac68f6892474fb80c87
Morgan Cabral <morgan.cabral@gmail.com>
2019-04-27 22:47:43 -0700

    Pushing up WIP for time controller stuff and mainly the material so James can make the wall look good (tm)

---
+++ ProjectAlmond/Assets/Scripts/TimeController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeController : MonoBehaviour
+{
+    public GameObject clock;
+
+    // Use this for initialization
+    void Start()
+    {
+        // todo
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // todo
+    }
+}
