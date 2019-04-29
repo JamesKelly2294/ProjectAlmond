53254faeddac338c533ceb70ff5c1a5fe25743e3
James Kelly <dxk2294@rit.edu>
2019-04-28 17:03:58 -0700

    asset pipeline for reagents

---
+++ ProjectAlmond/Assets/Scripts/ReagentBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ReagentBehavior : MonoBehaviour
+{
+    public ReagentData reagentData;
+
+    public MeshRenderer reagentRenderer;
+    public TextMeshPro productNameLabel;
+    public TextMeshPro flavorTextLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        reagentRenderer.material = new Material(reagentRenderer.material);
+        OnReagentValidation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnReagentValidation()
+    {
+        if(!Application.isPlaying)
+        {
+            return;
+        }
+
+        Color finalColor = reagentData.color * Mathf.LinearToGammaSpace(reagentData.emissiveness);
+        reagentRenderer.sharedMaterial.SetColor("_EmissionColor", finalColor);
+
+        reagentRenderer.sharedMaterial.color = reagentData.color;
+        productNameLabel.text = reagentData.productName;
+        flavorTextLabel.text = reagentData.flavorText;
+    }
+
+    void OnValidate()
+    {
+        reagentData.OnValidation.AddListener(OnReagentValidation);
+    }
+}
---
+++ ProjectAlmond/Assets/Scripts/ReagentData.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum AlleleType
+{
+    HeatResistance,
+    Aggression,
+    radiationResistance,
+    GrowRate,
+    ColdResistance,
+    Mobility,
+    Size,
+    Unused
+}
+
+public enum AlleleEffect
+{
+    Increase,
+    Decrease,
+    Random
+}
+
+public enum AlleleStrength
+{
+    Strong,
+    Weak,
+    Random
+}
+
+[System.Serializable]
+public struct AlleleModifier
+{
+    public AlleleType type;
+    public AlleleEffect effect;
+    public AlleleStrength strength;
+}
+
+[System.Serializable]
+public struct EquippedItem
+{
+    public long id;
+    public string type;
+    public string name;
+    public string equipmentSlot;
+}
+
+[CreateAssetMenu(fileName = "Reagent", menuName = "Reagent", order = 1)]
+public class ReagentData : ScriptableObject
+{
+    public string productName = "Reagent A";
+    public string flavorText = "Yeet";
+    public Color color = Color.red;
+    public float emissiveness = 0.1f;
+    public List<AlleleModifier> modifiers;
+
+    [HideInInspector]
+    public UnityEvent OnValidation;
+
+    public void OnValidate()
+    {
+        OnValidation.Invoke();
+    }
+}
